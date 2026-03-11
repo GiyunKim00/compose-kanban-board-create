da55d2e2b15ac193aaa76a3319cc746a847d69cf
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-11 13:46:12 +0900

    feat : CardCreationPanel 제목 입력 섹션 작성

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/CardCreationPanel.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/CardCreationPanel.kt
@@ -2,16 +2,20 @@ package woowacourse.kanban.board.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,6 +49,20 @@ fun CardCreationPanel(
             CardCreationPanelHeaderSection(
                 onShowCardCreationPanel = onShowCardCreationPanel,
             )
+
+            HorizontalDivider(modifier = Modifier.fillMaxWidth())
+
+            Column(
+                modifier = Modifier.padding(24.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+            ) {
+                CardCreationPanelSection(
+                    title = "제목 *",
+                    placeholder = "태스크 제목을 입력하세요",
+                    value = taskTitle,
+                    onTextChange = { taskTitle = it },
+                )
+            }
         }
 
     }
@@ -73,4 +92,65 @@ private fun CardCreationPanelHeaderSection(
             modifier = Modifier.clickable { onShowCardCreationPanel(false) },
         )
     }
+}
+
+@Composable
+private fun CardCreationPanelSection(
+    title: String,
+    modifier: Modifier = Modifier,
+    placeholder: String = "",
+    value: String,
+    onTextChange: (String) -> Unit = {},
+    showAdditionalInfo: Boolean = false,
+    infoText: String = "5자 이내의 태그를 최대 5개까지 등록할 수 있습니다.",
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        TitleText(title)
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = value,
+            onValueChange = { onTextChange(it) },
+            placeholder = {
+                Text(
+                    text = placeholder,
+                    color = Color(0xFFAAAAAA),
+                    fontSize = 16.sp,
+                    lineHeight = 24.sp,
+                    letterSpacing = 1.sp,
+                )
+            },
+            textStyle = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                letterSpacing = 1.sp,
+            ),
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+        )
+        if (showAdditionalInfo) {
+            Text(
+                text = infoText,
+                color = Color(0xFF49454F),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W400,
+                lineHeight = 16.sp,
+                modifier = Modifier.padding(top = 4.dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun TitleText(
+    title: String,
+) {
+    Text(
+        text = title,
+        fontSize = 14.sp,
+        color = Color(0xFF364153),
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        letterSpacing = 0.15.sp,
+    )
 }
