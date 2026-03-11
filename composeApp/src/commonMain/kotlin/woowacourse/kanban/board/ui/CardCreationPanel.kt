0c0832160e8b8dcf34ab24fb197802d640acc34c
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-11 13:45:11 +0900

    feat : CardCreationPanel 헤더 섹션 작성

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/CardCreationPanel.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/CardCreationPanel.kt
@@ -1,13 +1,17 @@
 package woowacourse.kanban.board.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,80 +20,57 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.kanban.board.domain.CardData
 
 @Composable
 fun CardCreationPanel(
     onAddItem: (CardData) -> Unit,
-    onShowInputWindow: (Boolean) -> Unit,
+    onShowCardCreationPanel: (Boolean) -> Unit,
 ) {
-    var title by remember { mutableStateOf("LazyColumn 컴포넌트 구현") }
+    var taskTitle by remember { mutableStateOf("") }
     var contents by remember { mutableStateOf("") }
-    var tempTag by remember { mutableStateOf("") }
-    var tags by remember { mutableStateOf(listOf<String>()) }
-    var author by remember { mutableStateOf("다이노") }
+    var tags by remember { mutableStateOf("") }
+    var state by remember { mutableStateOf("To Do") }
+    var manager by remember { mutableStateOf("다이노") }
 
-    OutlinedCard(modifier = Modifier.padding(10.dp)) {
-        Column(modifier = Modifier.padding(10.dp)) {
-            // 제목 입력
-            OutlinedTextField(
-                value = title,
-                onValueChange = { title = it },
-                label = { Text("제목 입력") },
-                modifier = Modifier.fillMaxWidth(),
+    OutlinedCard {
+        Column(
+            modifier = Modifier.background(Color.White).width(672.dp),
+        ) {
+            CardCreationPanelHeaderSection(
+                onShowCardCreationPanel = onShowCardCreationPanel,
             )
-
-            // 내용 입력
-            OutlinedTextField(
-                value = contents,
-                onValueChange = { contents = it },
-                label = { Text("내용 입력") },
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            // 태그 입력
-            OutlinedTextField(
-                value = tempTag,
-                onValueChange = { tempTag = it },
-                label = { Text("태그 입력 후 엔터") },
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        if (tempTag.isNotBlank() && !tags.contains(tempTag)) {
-                            tags = tags + tempTag.trim()
-                            tempTag = ""
-                        }
-                    },
-                ),
-            )
-
-            /**
-             * 작성자 입력
-             * @param author 작성자
-             * @param onAuthorChange 작성자 변경 콜백
-             * @param onAddItem 추가 콜백
-             */
-            OutlinedTextField(
-                value = author,
-                onValueChange = { author = it },
-                label = { Text("작성자 입력") },
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            // 확인 버튼
-            Button(
-                onClick = {
-                    onAddItem(CardData.create(title, contents, tags, author))
-                    onShowInputWindow(false)
-                },
-                modifier = Modifier.align(Alignment.End),
-            ) {
-                Text("확인")
-            }
         }
+
+    }
+}
+
+@Composable
+private fun CardCreationPanelHeaderSection(
+    onShowCardCreationPanel: (Boolean) -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 28.dp, horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = "새 태스크 생성",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.W600,
+            lineHeight = 28.sp,
+            letterSpacing = (-0.45).sp,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = "닫기 아이콘",
+            modifier = Modifier.clickable { onShowCardCreationPanel(false) },
+        )
     }
 }
