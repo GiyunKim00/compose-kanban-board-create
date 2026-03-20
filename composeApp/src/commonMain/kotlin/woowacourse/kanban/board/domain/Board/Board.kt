22d2bfb4dda2d59ae01fdb15a3a951eef85d9092
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-20 17:17:50 +0900

    feat : Board 프로그레스 바, add, update, delete 로직 작성

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Board/Board.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Board/Board.kt
@@ -15,4 +15,20 @@ class Board(
     fun inProgressTaskCount(): Int = cardList.count { it.taskState == CardTaskState.IN_PROGRESS}
 
     fun toDoTaskCount(): Int = cardList.count { it.taskState == CardTaskState.TODO}
+
+    fun completionRate() : Int {
+        return if(totalTaskCount() == 0) 0
+        else doneTaskCount()* 100 / totalTaskCount()
+    }
+
+    fun addCard(card: Card): Board = Board(cardList + card)
+
+    fun deleteCard(card: Card): Board = Board(cardList - card)
+
+    fun updateCard(targetCard: Card, updatedCard: Card): Board = Board(
+        cardList.map {
+            if (it.id == targetCard.id) updatedCard
+            else it
+        }
+    )
 }
