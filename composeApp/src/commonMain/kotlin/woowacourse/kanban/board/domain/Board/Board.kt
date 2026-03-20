32f8d16a9e4d29bcb79313552d1fca6aa36011ee
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-20 17:16:28 +0900

    feat : Board 내 Card 관리 및 전체 개수 로직 작성

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Board/Board.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Board/Board.kt
@@ -10,28 +10,4 @@ class Board(
 
     fun totalTaskCount(): Int = cardList.size
 
-    fun doneTaskCount(): Int = cardList.count { it.state == CardTaskState.DONE }
-
-    fun inProgressTaskCount(): Int = cardList.count { it.state == CardTaskState.IN_PROGRESS}
-
-    fun toDoTaskCount(): Int = cardList.count { it.state == CardTaskState.TODO}
-
-    fun completionRate() : Int {
-        return if(totalTaskCount() == 0) 0
-        else doneTaskCount()* 100 / totalTaskCount()
-    }
-
-    fun addCard(card: Card): Board = Board(cardList + card)
-
-    fun deleteCard(card: Card): Board = Board(cardList - card)
-
-    fun updateCard(targetCard : Card, updatedCard : Card) : Board = Board(
-        cardList.map {
-            if(it == targetCard) updatedCard
-            else it
-        }
-    )
-
-    fun countByState(state: CardTaskState): Int = cardList.count { it.state == state }
-
 }
